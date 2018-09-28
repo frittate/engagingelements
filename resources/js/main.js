3687921335041e601c709d472ef3a888f38ac23a
Sebastian Martin <acedia247@gmail.com>
2018-09-28 14:48:37 +0200

    inserted jquery and particles

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,7 @@
 particlesJS.load('particles-js', './../../vendors/js/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+  particlesJS.load('particles-js2', './../../vendors/js/particlesjs-config.json', function() {
+    console.log('callback - particles.js 2 config loaded');
+  });
