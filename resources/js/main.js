a6c93f4c22e08fc7e947db1c6c99f0553bb6cb6e
Sebastian Martin <acedia247@gmail.com>
2018-10-11 11:25:10 +0200

    orange_commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,3 +5,23 @@ particlesJS.load('particles-js', './../../vendors/js/particlesjs-config.json', f
   particlesJS.load('particles-js2', './../../vendors/js/particlesjs-config.json', function() {
     console.log('callback - particles.js 2 config loaded');
   });
+
+  $(document).ready(function(){
+    let mobileNav = false;
+
+    $('#nav-burger').click(function() {
+      if (mobileNav === false) {
+        $('.fa-bars').addClass('fa-times').removeClass('fa-bars');
+        $('.nav-items').css('display', 'flex');
+        mobileNav = true;
+      } else {
+        $('.fa-times').addClass('fa-bars').removeClass('fa-times');
+        $('.nav-items').css('display', 'none');
+        mobileNav = false;
+      }
+      
+    })
+    
+    // jQuery methods go here...
+ 
+ });
